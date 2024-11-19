bd4ccb35eba98e29bc1cc6d484d3c5b9fa4c6451
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2024-11-19 10:59:41 +0100

    fix: codestyle

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -72,9 +72,9 @@ if not config["clock"]:
 
 # Make sure the target is unique for auto label
 # see: https://docs.readthedocs.io/en/stable/guides/cross-referencing-with-sphinx.html
-# autosectionlabel_prefix_document = True
+autosectionlabel_prefix_document = True
 suppress_warnings = [
-    'autosectionlabel.*',
+    "autosectionlabel.*",
 ]
 
 # Intersphinx mapping

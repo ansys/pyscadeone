2a268241161d91998e3d7c5637f05cbc0001e094
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2024-11-19 10:51:23 +0100

    fix: supress verbose

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -72,7 +72,7 @@ if not config["clock"]:
 
 # Make sure the target is unique for auto label
 # see: https://docs.readthedocs.io/en/stable/guides/cross-referencing-with-sphinx.html
-autosectionlabel_prefix_document = True
+# autosectionlabel_prefix_document = True
 suppress_warnings = [
     'autosectionlabel.*',
 ]

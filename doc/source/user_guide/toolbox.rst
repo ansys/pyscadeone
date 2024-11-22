d022ea7af40414e8fffff80766d9b3d6cc9126c6
Xavier Fornari <104919875+xfornari@users.noreply.github.com>
2024-11-22 15:34:17 +0100

    docs: clean unused .rst, restore -W opt

--- doc/source/user_guide/toolbox.rst
+++
@@ -1,5 +0,0 @@
-Integration Toolbox
-===================
-
-.. note::
-    TODO: To be completed when mapping file ready
--- src/ansys/scadeone/core/__init__.py
+++ src/ansys/scadeone/core/__init__.py
@@ -1,12 +1,31 @@
-# Copyright (c) 2022-2024 ANSYS, Inc.
-# Unauthorized use, distribution, or duplication is prohibited.
+# Copyright (c) 2022 - 2024 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 from pathlib import Path
 from collections import namedtuple
 from platformdirs import PlatformDirs
 
 # Version must be directly defined for flit. No computation, else flit will fails
-__version__ = "0.7.0"
+__version__ = "0.6.dev1"
 
 Version = namedtuple("Version", ["major", "minor", "patch", "build"])
 (M, m, p) = __version__.split(".")

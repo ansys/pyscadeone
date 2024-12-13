0b296e5d89291a949ff984d9402c5748e335c2eb
Xavier Fornari <104919875+xfornari@users.noreply.github.com>
2024-12-13 11:39:29 +0100

    doc: numpydoc is needed for clean doc rendering.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -45,10 +45,10 @@ copybutton_prompt_is_regexp = True
 # Sphinx extensions
 
 extensions = [
+    "numpydoc",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.autosectionlabel",
-    #    "numpydoc",
     "sphinx.ext.intersphinx",
     "sphinx_copybutton",
     "sphinx.ext.inheritance_diagram",
@@ -82,26 +82,10 @@ intersphinx_mapping = {
 }
 
 ## numpydoc configuration
-# numpydoc_show_class_members = False
-# numpydoc_xref_param_type = True
+# Note: this is key for autosummary, else it fails.
+numpydoc_show_class_members = False
+numpydoc_xref_param_type = True
 #
-## Consider enabling numpydoc validation. See:
-## https://numpydoc.readthedocs.io/en/latest/validation.html#
-# numpydoc_validate = True
-# numpydoc_validation_checks = {
-#    "GL06",  # Found unknown section
-#    "GL07",  # Sections are in the wrong order.
-#    "GL08",  # The object does not have a docstring
-#    "GL09",  # Deprecation warning should precede extended summary
-#    "GL10",  # reST directives {directives} must be followed by two colons
-#    "SS01",  # No summary found
-#    "SS02",  # Summary does not start with a capital letter
-#    # "SS03", # Summary does not end with a period
-#    "SS04",  # Summary contains heading whitespaces
-#    # "SS05", # Summary must start with infinitive verb, not third person
-#    "RT02",  # The first line of the Returns section should contain only the
-#    # type, unless multiple values are being returned"
-# }
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
